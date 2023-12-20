07c46f2907dc13af0b41d0368b42b187fb8f5928
Pongpisut Kongdan <Pongpisut.kon@bjc.co.th>
2023-12-20 09:19:39 +0700

    commit

--- Section7CollectionInC/10 - ArraysAsParameters/ArraysAsParameters/Program.cs
+++ Section7CollectionInC/10 - ArraysAsParameters/ArraysAsParameters/Program.cs
@@ -13,27 +13,36 @@ namespace ArraysAsParameters
             int[] studentsGrades = new int[] { 15, 13, 8, 12, 6, 16 };
             double averageResult = GetAverage(studentsGrades);
 
-            int[] happiness = { 2, 3, 4, 5, 6 };
-            SunIsShining(happiness);
-
-            foreach(int y in happiness)
-            {
-                Console.WriteLine(y);
-            }
-            Console.ReadLine();
-
-
-
-            foreach(int grade  in studentsGrades)
+            foreach (int grade in studentsGrades)
             {
                 Console.WriteLine(" {0} ", grade);
             }
 
             Console.WriteLine("The average is: {0}", averageResult);
             Console.ReadKey();
+
+            int[] happiness = { 2, 3, 4, 5, 6 };
+            SunIsShining(happiness);
+
+            // CHALLENGE - Create an array of happiness in the main method and assign 5 values to it.
+            // Create a method "SunIsShining", which has an array of type int as its parameter. 
+            // This method should increase the argument given by 2, for each entry.
+            // Call this method in the main method and use "happiness" as the argument.
+            // Create a foreach loop in the main method to write all values onto the console.
+
+            //foreach (int y in happiness)
+            //{
+            //    Console.WriteLine(y);
+            //}
+            //Console.ReadLine();
+            for (int i = 0; i < happiness.Length; i++)
+            {
+                Console.WriteLine(happiness[i]);
+            }
+            Console.ReadKey();
         }
 
-        static double GetAverage(int[] gradesArray)
+        static double GetAverage(params int[] gradesArray)
         {
             int size = gradesArray.Length;
             double average;
@@ -43,21 +52,18 @@ namespace ArraysAsParameters
             {
                 sum += gradesArray[i];
             }
-            average = (double)sum / size;
+            average = Math.Round((double)sum / size, 2);
             return average;
         }
 
-        static void SunIsShining(int[] x)
+        static void SunIsShining(params int[] x)
         {
-            for (int i = 0; i < x.Length; i++)
+            for(int i = 0; i < x.Length; i++)
+            {
                 x[i] += 2;
+            }
         }
 
 
-        // CHALLENGE - Create an array of happiness in the main method and assign 5 values to it.
-        // Create a method "SunIsShining", which has an array of type int as its parameter. 
-        // This method should increase the argument given by 2, for each entry.
-        // Call this method in the main method and use "happiness" as the argument.
-        // Create a foreach loop in the main method to write all values onto the console.
     }
 }
